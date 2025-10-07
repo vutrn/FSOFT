820d93e207aedd8c3cececc704de2ff5b0dffd8c
vutrn <meoiuwai@gmail.com>
2025-10-07 16:25:46 +0700

    reorganize

---
+++ script.js
@@ -0,0 +1,31 @@
+
+console.log("xin chào, đây là console.log");
+console.warn("day là console.warn");
+console.error("đây là lỗi! ")
+
+const user = {name: "Ashley", age: 30};
+console.log(user)
+
+const products = [
+    { id: 1, name: "Laptop", price: 999.99 },
+    { id: 2, name: "Smartphone", price: 499.99 },
+    { id: 3, name: "Tablet", price: 299.99 },
+];
+
+console.log(products [0]);
+
+let userName = "Ashley";
+const birthYear = 1993;
+var isStudent = true;
+
+userName = "John";  
+isStudent = false;
+
+console.log(userName);
+console.log(birthYear);
+console.log(isStudent);
+
+let num1 = 10;
+Number.isInteger(num1);
+
+console.log(typeof(NaN))
